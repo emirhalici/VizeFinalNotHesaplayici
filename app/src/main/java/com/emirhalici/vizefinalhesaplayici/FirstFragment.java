71c7f5375530dd702668903791bd139071cbf387
emirhalici1 <emir.halici1@gmail.com>
2021-04-21 00:48:21 +0300

    Added grade calculation, grade needed for A calculation and set textview accordingly to calculations. App is now a working prototype.

--- app/src/main/java/com/emirhalici/vizefinalhesaplayici/FirstFragment.java
+++ app/src/main/java/com/emirhalici/vizefinalhesaplayici/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.emirhalici.vizefinalhesaplayici;
 
+import android.bluetooth.le.ScanSettings;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,6 +18,9 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class FirstFragment extends Fragment {
+
+    int MIDTERM_MULTIPLIER = 30;
+    int FINAL_MULTIPLIER = 80;
 
     @Override
     public View onCreateView(
@@ -29,19 +34,52 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        view.findViewById(R.id.button_first).setOnClickListener(view1 -> NavHostFragment.findNavController(FirstFragment.this)
+                .navigate(R.id.action_FirstFragment_to_SecondFragment));
 
         TextInputLayout tilVize = view.findViewById(R.id.til_vize);
         TextInputLayout tilFinal = view.findViewById(R.id.til_final);
         SeekBar sbVize = view.findViewById(R.id.sb_vize);
         SeekBar sbFinal = view.findViewById(R.id.sb_final);
+        TextView tv = view.findViewById(R.id.tv);
 
+
+        // TextInputLayout - seekBar logic to link the value to each other.
+        // i.e. when sbVize gets changed by user, tilVize text is changed accordingly.
+        sbVize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tilVize.getEditText().setText(String.valueOf(progress));
+                getAndSetGrades(sbVize, sbFinal, tv);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        sbFinal.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tilFinal.getEditText().setText(String.valueOf(progress));
+                getAndSetGrades(sbVize, sbFinal, tv);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         tilVize.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -51,12 +89,15 @@ public class FirstFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 tilVize.setError(getString(R.string.GradeOutOfBorder));
-                s = s.toString()=="" ? "0" : s.toString();
-                if (s.toString()=="" || s.toString().isEmpty()) {s="0";}
+                s = s.toString().equals("") ? "0" : s.toString();
+                if (s.toString().equals("") || s.toString().isEmpty()) {s="0";}
                 boolean isErrorEnabled = Integer.parseInt(s.toString()) > 100;
                 tilVize.setErrorEnabled(isErrorEnabled);
                 if (!isErrorEnabled) {
                     sbVize.setProgress(Integer.parseInt(s.toString()));
+                    getAndSetGrades(sbVize, sbFinal, tv);
+                } else {
+                    tv.setText("");
                 }
             }
 
@@ -65,7 +106,6 @@ public class FirstFragment extends Fragment {
 
             }
         });
-
         tilFinal.getEditText().addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -75,12 +115,15 @@ public class FirstFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 tilFinal.setError(getString(R.string.GradeOutOfBorder));
-                s = s.toString()=="" ? "0" : s.toString();
-                if (s.toString()=="" || s.toString().isEmpty()) {s="0";}
+                s = s.toString().equals("") ? "0" : s.toString();
+                if (s.toString().equals("") || s.toString().isEmpty()) {s="0";}
                 boolean isErrorEnabled = Integer.parseInt(s.toString()) > 100;
                 tilFinal.setErrorEnabled(isErrorEnabled);
                 if (!isErrorEnabled) {
                     sbFinal.setProgress(Integer.parseInt(s.toString()));
+                    getAndSetGrades(sbVize, sbFinal, tv);
+                } else {
+                    tv.setText("");
                 }
             }
 
@@ -90,4 +133,59 @@ public class FirstFragment extends Fragment {
             }
         });
     }
+
+    void getAndSetGrades(SeekBar sbMidterm, SeekBar sbFinal, TextView textView) {
+        int midtermGrade = sbMidterm.getProgress();
+        int finalGrade = sbFinal.getProgress();
+        int grade = calculateGrade(midtermGrade, finalGrade, MIDTERM_MULTIPLIER, FINAL_MULTIPLIER);
+        String gradeLetter = getLetterGrade(grade);
+        int gradeToA = gradeNeededForA(midtermGrade, MIDTERM_MULTIPLIER, FINAL_MULTIPLIER);
+        setGradeToView(grade, finalGrade, gradeToA, gradeLetter, textView);
+    }
+
+    void setGradeToView(int grade, int finalGrade, int gradeForA, String letterGrade, TextView textView) {
+        String tv_string = String.format("Grade: %d\nGrade letter: %s\n", grade, letterGrade);
+        if (finalGrade>=gradeForA) {
+            tv_string = tv_string + "Congrats on that A.";
+        } else if (gradeForA>100) {
+            tv_string = tv_string + "Getting an A is impossible at this moment.";
+        } else if (gradeForA>0) {
+            tv_string = tv_string + String.format("To get an A, final grade should be at least: %d\n", gradeForA);
+        }
+        textView.setText(tv_string);
+    }
+
+    String getLetterGrade(int grade) {
+        String letterGrade;
+        if (grade>=100) {
+            letterGrade = "A+";
+        } else if (grade>=90) {
+            letterGrade = "A";
+        } else if (grade>=85) {
+            letterGrade = "B1";
+        } else if (grade>=80) {
+            letterGrade = "B2";
+        } else if (grade>=75) {
+            letterGrade = "B3";
+        } else if (grade>=70) {
+            letterGrade = "C1";
+        } else if (grade>=65) {
+            letterGrade = "C2";
+        } else if (grade>=60) {
+            letterGrade = "C3";
+        } else {
+            letterGrade = "F";
+        }
+        return letterGrade;
+    }
+
+    int gradeNeededForA(int midtermGrade, int midtermMultiplier, int finalMultiplier) {
+        float gradeNeededF = ( ( (float) 90 -  (float) midtermGrade * midtermMultiplier / 100 ) / ( (float) finalMultiplier / 100 ));
+        return (int) gradeNeededF;
+    }
+
+    int calculateGrade(int midtermGrade, int finalGrade, int midtermMultiplier, int finalMultiplier) {
+        return midtermGrade * midtermMultiplier / 100 + finalGrade * finalMultiplier / 100;
+    }
+
 }
--- app/src/main/java/com/emirhalici/vizefinalhesaplayici/MainActivity.java
+++ app/src/main/java/com/emirhalici/vizefinalhesaplayici/MainActivity.java
@@ -23,13 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setVisibility(View.GONE);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 
     @Override
