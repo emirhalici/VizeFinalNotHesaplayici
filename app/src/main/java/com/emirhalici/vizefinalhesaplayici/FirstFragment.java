dc8c1e751f919ac7b440cf0b79aa0f693cb537a6
emirhalici1 <emir.halici1@gmail.com>
2021-04-20 19:16:59 +0300

    Initial commit Initial layout changes to first fragment InputLayout Logic worked out

--- app/src/main/java/com/emirhalici/vizefinalhesaplayici/FirstFragment.java
+++ app/src/main/java/com/emirhalici/vizefinalhesaplayici/FirstFragment.java
@@ -1,13 +1,19 @@
 package com.emirhalici.vizefinalhesaplayici;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class FirstFragment extends Fragment {
 
@@ -28,6 +34,59 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        TextInputLayout tilVize = view.findViewById(R.id.til_vize);
+        TextInputLayout tilFinal = view.findViewById(R.id.til_final);
+        SeekBar sbVize = view.findViewById(R.id.sb_vize);
+        SeekBar sbFinal = view.findViewById(R.id.sb_final);
+
+        tilVize.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tilVize.setError(getString(R.string.GradeOutOfBorder));
+                s = s.toString()=="" ? "0" : s.toString();
+                if (s.toString()=="" || s.toString().isEmpty()) {s="0";}
+                boolean isErrorEnabled = Integer.parseInt(s.toString()) > 100;
+                tilVize.setErrorEnabled(isErrorEnabled);
+                if (!isErrorEnabled) {
+                    sbVize.setProgress(Integer.parseInt(s.toString()));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        tilFinal.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tilFinal.setError(getString(R.string.GradeOutOfBorder));
+                s = s.toString()=="" ? "0" : s.toString();
+                if (s.toString()=="" || s.toString().isEmpty()) {s="0";}
+                boolean isErrorEnabled = Integer.parseInt(s.toString()) > 100;
+                tilFinal.setErrorEnabled(isErrorEnabled);
+                if (!isErrorEnabled) {
+                    sbFinal.setProgress(Integer.parseInt(s.toString()));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
