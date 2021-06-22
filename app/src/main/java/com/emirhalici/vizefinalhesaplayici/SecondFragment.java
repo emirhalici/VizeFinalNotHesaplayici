99d4c2bb5213a635ae31417b9826f3ed8662ef18
emirhalici <emir.halici1@gmail.com>
2021-06-22 18:46:02 +0300

    new fragment to edit multipliers

--- app/src/main/java/com/emirhalici/vizefinalhesaplayici/SecondFragment.java
+++ app/src/main/java/com/emirhalici/vizefinalhesaplayici/SecondFragment.java
@@ -8,11 +8,13 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class SecondFragment extends Fragment {
@@ -31,16 +33,49 @@ public class SecondFragment extends Fragment {
         view.findViewById(R.id.button_second).setOnClickListener(view1 -> NavHostFragment.findNavController(SecondFragment.this)
                 .navigate(R.id.action_SecondFragment_to_FirstFragment));
 
+        String VIZEMULTIPLIER = "vizeMultiplier";
+        String FINALMULTIPLIER = "finalMultiplier";
+
         TextInputLayout tilvize = view.findViewById(R.id.til_vizemult);
         TextInputLayout tilfinal = view.findViewById(R.id.til_finalmult);
 
         SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        int vizeM = sharedPref.getInt("vizeMultiplier", 30);
-        int finalM = sharedPref.getInt("finalMultiplier", 80);
+        int vizeM = sharedPref.getInt(VIZEMULTIPLIER, 30);
+        int finalM = sharedPref.getInt(FINALMULTIPLIER, 80);
 
         tilvize.getEditText().setText(String.valueOf(vizeM));
         tilfinal.getEditText().setText(String.valueOf(finalM));
 
+        FloatingActionButton fab = view.findViewById(R.id.fab_save);
+
+        fab.setOnClickListener(v -> {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            boolean fail = false;
+            try {
+                tilvize.setErrorEnabled(false);
+                int vizemult = Integer.parseInt(tilvize.getEditText().getText().toString());
+                editor.putInt(VIZEMULTIPLIER, vizemult);
+            } catch (NumberFormatException e) {
+                tilvize.setError("Please enter a valid number");
+                tilvize.setErrorEnabled(true);
+                fail = true;
+            }
+
+            try {
+                tilfinal.setErrorEnabled(false);
+                int finalmult = Integer.parseInt(tilfinal.getEditText().getText().toString());
+                editor.putInt(FINALMULTIPLIER, finalmult);
+            } catch (NumberFormatException e) {
+                tilfinal.setError("Please enter a valid number");
+                tilfinal.setErrorEnabled(true);
+                fail = true;
+            }
+            if (!fail) {
+                editor.apply();
+                Toast.makeText(getContext(),"Multiplier is set successfully", Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
     }
 }
